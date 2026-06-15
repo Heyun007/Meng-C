5b4a7b0d29bd83b8fe031d1966cf0fbf25ad7216
Heyun007 <3170088497@qq.com>
2026-06-15 07:27:53 +0000

    update

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+self.addEventListener('push', e => {
+  const data = e.data ? e.data.json() : {title:'梦角查岗',body:'来消息了'};
+  e.waitUntil(self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: data.icon || '',
+    tag: 'dream-' + Date.now(),
+    requireInteraction: true
+  }));
+});
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  clients.openWindow('/');
+});
